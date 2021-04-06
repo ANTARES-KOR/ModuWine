5bc74aec2ace39e0bea62642a2a107103fc8bde9
ANTARES-KOR <75557859+ANTARES-KOR@users.noreply.github.com>
2021-04-07 01:19:58 +0900

    체크박스 추가
    마지막 이메일 제출 폼에 체크박스 추가
    버튼 누르면 해당 체크박스 자동체크기능 제작

--- script/ctaButtonClick.js
+++ script/ctaButtonClick.js
@@ -1,9 +1,19 @@
-const ctaBtnArr = document.querySelectorAll(".cta-btn");
+const ctaBtnArr = document.querySelectorAll('.cta-btn');
 function handleCtaBtnClick(e) {
-    window.scrollTo(0, 1500);
-    const emailInput = document.querySelector(".email-input");
-    emailInput.focus();
+  window.scrollTo(0, 1500);
+  if (e.target.classList.contains('wine-service-btn')) {
+    const checkBoxA = document.querySelector('.specialDay');
+    checkBoxA.checked = true;
+  } else if (e.target.classList.contains('wine-sampler-btn')) {
+    const checkBoxB = document.querySelector('.wineSampler');
+    checkBoxB.checked = true;
+  } else {
+    const checkBoxC = document.querySelector('.wineTest');
+    checkBoxC.checked = true;
+  }
+  const emailInput = document.querySelector('.email-input');
+  emailInput.focus();
 }
 ctaBtnArr.forEach(function (btn) {
-    btn.addEventListener("click", handleCtaBtnClick);
+  btn.addEventListener('click', handleCtaBtnClick);
 });
